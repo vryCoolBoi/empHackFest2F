8f9ef2f1b6e267dd6a7919540329c2668e596119
vryCoolBoi <ilaxman.2022@gmail.com>
2026-09-06 14:07:03 -0700

    Maps better+ Safty added

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.get("/signup", function (req, res) {
 
 app.post("/submit", upload.single("evidence"), function (req, res) {
   if (!currentUser) {
+    return
   }
   let incident_type = req.body["incident-type"];
   let description = req.body["report_description"];
